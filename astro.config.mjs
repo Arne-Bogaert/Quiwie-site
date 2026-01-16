e4ed066ccff55ff672a70c81d647c3a881ef3801
Arne Bogaert <arne.bogaert@student.hogent.be>
2026-01-16 20:50:34 +0100

    nog eens

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://Arne-Bogaert.github.io',
   base: '/quiwie-site',
