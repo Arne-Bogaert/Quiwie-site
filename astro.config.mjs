ec47d90ca0564ad816c8d6d5343f0eaa14abe890
Arne Bogaert <arne.bogaert@student.hogent.be>
2026-01-16 20:47:30 +0100

    Configuration for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Arne-Bogaert.github.io',
+  base: '/quiwie-site',
+});
